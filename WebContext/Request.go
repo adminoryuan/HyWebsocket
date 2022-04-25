d50d4c315ffaed6d9241cb485b81aae79e801a78
adminoryuan <1317332365@qq.com>
2022-04-25 12:25:59 +0800

    Bind

--- WebContext/Request.go
+++ WebContext/Request.go
@@ -1,10 +1,17 @@
 package Webcontext
 
-import "net"
+import (
+	"encoding/json"
+	"net"
+)
 
 //描述一个用户端发送数据
 type RequestConn struct {
 	LocalRemoter net.IP
 
 	Bodys []byte
+}
+
+func (r *RequestConn) Bind(o interface{}) error {
+	return json.Unmarshal(r.Bodys, o)
 }
